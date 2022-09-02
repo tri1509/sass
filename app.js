93a0c980a7ec5bef416dde79bfa7b6b66f718aa0
Minh Tri <tangminhtri1592000@gmail.com>
2022-09-02 18:19:24 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// const menuToggle = document.querySelector(".menu-toggle");
+// const menu = document.querySelector(".header-menu");
+// menuToggle.addEventListener("click", function(){
+//     menu.classList.add("is-active");
+// });
+// document.addEventListener("click", function(e){
+//     if(!menu.contains(e.target) && !e.target.matches(".menu-toggle")) {
+//         menu.classList.remove("is-active");
+//     };
+// })
